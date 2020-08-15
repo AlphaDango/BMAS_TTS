b2c7032a34b03fda268940ea5034939054c47096
AlphaDango <on18@tu-clausthal.de>
2020-08-15 23:14:52 +0200

    changed runnable to lamnda expression in AudioPlayer.java

--- src/main/java/de/neumann/main/Main.java
+++ src/main/java/de/neumann/main/Main.java
@@ -4,7 +4,6 @@ import de.neumann.audioPlayer.AudioPlayer;
 
 public class Main {
     public static void main(String[] args) {
-        AudioPlayer audioPlayer = new AudioPlayer();
-        audioPlayer.playSound("kek");
+        AudioPlayer.playSound("intruder detected use of weapon authorized");
     }
 }
